14e01a8aab7786bc850bdebf3069a9aea945fdfa
Ulises Rey <ulises.rey@univie.ac.at>
2024-11-11 11:09:23 +0100

    added random user agents and random time delays

--- source/scrape_guardian.py
+++ source/scrape_guardian.py
@@ -7,6 +7,20 @@ Module to scrape the Guardian newspaper website for headlines on a given date.
 
 import requests
 from bs4 import BeautifulSoup
+import random
+import time
+
+# We define some agents, so we can scrap with different agents later
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/89.0",
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.361675787110',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5412.99 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5361.172 Safari/537.36',
+    'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5388.177 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5397.215 Safari/537.36'
+]   
 
 def scrape_guardian_headlines(date):
     """
@@ -17,7 +31,8 @@ def scrape_guardian_headlines(date):
     url = f"https://www.theguardian.com/world/{date_str}"
   
     # Define a user agent to avoid 403 error
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': random.choice(USER_AGENTS)}
+
 
     try:
         response = requests.get(url, headers=headers)
@@ -35,5 +50,9 @@ def scrape_guardian_headlines(date):
         headline_text = article.get_text().strip()
         if headline_text:
             headlines.append(headline_text)
+
+
+    # Add a random delay to mimic human behavior
+    time.sleep(random.uniform(1, 3))  # Random delay between 1 and 3 seconds
     
     return headlines
