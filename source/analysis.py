724293ce35d767607dfee7a43c3bc2997762d58b
Ulises Rey <ulises.rey@univie.ac.at>
2024-11-11 15:20:57 +0100

    added analysis module, deleted unused utils.py

---
+++ source/analysis.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def compare_two_words(df, word1, word2):
+    """
+    Compare the occure of two words in the headlines over time
+    """
+    df[word1] = df['headline'].str.contains(word1, case=False)
+    df[word2] = df['headline'].str.contains(word2, case=False)
+    df['month'] = df['date'].dt.to_period('M')
+    df.groupby('month')[word1].sum().plot()
+    df.groupby('month')[word2].sum().plot()
+    plt.title(f"Comparing {word1} and {word2}")
+    plt.ylabel("Number of Headlines per month")
+    plt.xlabel("Time")
+    # add legend
+    plt.legend([word1, word2])
+    plt.show()
+
+
+def compare_occurence(df, *words):
+    """
+    Compare the occurence of multiple words in the headlines over time
+    """
+    for word in words:
+        df[word] = df['headline'].str.contains(word, case=False)
+    df['month'] = df['date'].dt.to_period('M')
+    for word in words:
+        df.groupby('month')[word].sum().plot()
+    plt.title(f"Comparing {', '.join(words)}")
+    # labels
+    plt.ylabel("Number of Headlines per month")
+    plt.xlabel("Time")
+    # add legend
+    plt.legend(words)
+    plt.show()
